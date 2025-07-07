3a5bd08fc6b308b484ea8c7cd0425dce0fb5c902
guy friedman <guy.friedman2001@gmail.com>
2025-07-07 11:11:30 +0300

    Update malloc_2.cpp

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -1,179 +1,101 @@
-#define SYSCALL_FAILED(POINTER) (((long int) POINTER) == -1)
-//#define BYTES_IN_ARCHITECTURE (4) //4 if we are in a 32 bit machine, if in 64 bit then it should be 8
-#define EXTRA_DATA_AMMOUNT (4) //ammount of actual variables we would need to allocate
-
-#define ARCH_SIZE (sizeof(void*)) //getting tired of writing sizeof(void*) everywhere, made a define for easier use
-
-
-enum MemoryStatus {
-    FREE = 0,
-    ALLOCATED = 1
-};
-
 typedef unsigned long size_t; //FIXME: delete this line! its only for my mac
+void* smalloc(size_t size);
+void* scalloc(size_t num, size_t size);
+void sfree(void* p);
+void* srealloc(void* oldp, size_t size);
+size_t _num_free_blocks();
+size_t _num_free_bytes();
+size_t _num_allocated_blocks();
+size_t _num_allocated_bytes();
+size_t _num_meta_data_bytes();
+size_t _size_meta_data();
 
-inline bool _isFlag(void* block, MemoryStatus flag){
-    if (!block){
-        //make it safe to call on nullptr, similar behaviour to dev/null
-        //as per the tutorials or lectures or whatever
-        return true;
-    }
-    //go 2 'objects' below the pointer
-    void* address_2_below = (block-2*ARCH_SIZE);
-
-    size_t value_at_addres_2_below = *((size_t*)address_2_below);
-    
-
-    //check if last bit is 1 or 0
-    return ((value_at_addres_2_below&flag)==flag);
-}
-
-inline bool isAllocated(void* block){
-    return _isFlag(block, ALLOCATED);
-}
-
-#if 0 // i dont think we need this function
-inline bool isFree(void* block){
-    return _isFlag(block, FREE);
-}
-#endif
-
-#if 0 //didnt find a good way to do it
-inline void _apply_to_flag(void* block, MemoryStatus flag){
-        if (!block){
-        //make it safe to call on nullptr, similar behaviour to dev/null
-        //as per the tutorials or lectures or whatever
-        return;
-    }
-
-    //go 2 'objects' below the pointer
-    size_t* address_2_below = (size_t*) (block-2*ARCH_SIZE);
-
-    ((*address_2_below)&=flag)|=flag;
-}
-#endif
-
-inline void markAllocated(void* block){
-    if (!block){
-        //make it safe to call on nullptr, similar behaviour to dev/null
-        //as per the tutorials or lectures or whatever
-        return;
-    }
-
-    //go 2 'objects' below the pointer
-    size_t* address_2_below = (size_t*) (block-2*ARCH_SIZE);
-
-    (*address_2_below)|=ALLOCATED;
-}
-
-inline void markFree(void* block){
-    if (!block){
-        //make it safe to call on nullptr, similar behaviour to dev/null
-        //as per the tutorials or lectures or whatever
-        return;
-    }
-
-    //go 2 'objects' below the pointer
-    size_t* address_2_below = (size_t*) (block-2*ARCH_SIZE);
-
-    //easiest way i found to make the first bit 0 without using a predefined register size,
-    //just make sure the first bit is 1 and then xor it with 1.
-    ((*address_2_below)|=ALLOCATED)^=ALLOCATED;
-}
-
-inline size_t getBlockSize(void* block){
-    if (!block){
-        //make it safe to call on nullptr, similar behaviour to dev/null
-        //as per the tutorials or lectures or whatever
-        return 0;
-    }
-
-    //go 2 'objects' below the pointer
-    size_t* address_2_below = (size_t*) (block-2*ARCH_SIZE);
-
-    size_t size = *address_2_below;
-
-    //easiest way i found to make the first bit 0 without using a predefined register size,
-    //just make sure the first bit is 1 and then xor it with 1.
-    ((size)|=ALLOCATED)^=ALLOCATED;
-
-    return size;
-}
-
-
-/**
-● Tries to allocate ‘size’ bytes.
-● Return value:
-i. Success –a pointer to the first allocated byte within the allocated block.
- ii. Failure –
-    a. If ‘size’ is 0 returns NULL.
-    b. If ‘size’ is more than 10**8, return NULL.
-    c. If sbrk fails, return NULL.  
-
-Notes:
-    ● size_t is a typedef to unsigned int in 32-bit architectures, and to unsigned long long in 64-bit
-      architectures. This means that trying to insert a negative value will result in compiler warning.
-    ● You do not need to implement free(), calloc() or realloc() for this section.
-      
-    Discussion: Before proceeding, try discussing the current implementation with your partner. What’s
-    wrong with it? What’s missing? Are we handling fragmentation? What would you do differently?
-*/
 void* smalloc(size_t size){
-    /**
-    Heap Overview:
+    /*
+    ● Searches for a free block with at least ‘size’ bytes or allocates (sbrk()) one if none are
+found.
+● Return value:
+i. Success – returns pointer to the first byte in the allocated block (excluding the meta-data of
+course)
+ii. Failure –
+a. If size is 0 returns NULL.
+b. If ‘size’ is more than 108, return NULL.
+c. If sbrk fails in allocating the needed space, return NULL. 
+    */
+}
 
-      XX--other data--XX
-        block size (long) //no allocation flag here!
-              |
-    next avalible block (long) //maintained only for 'free' blocks, points to the addres that was returned from the malloc call (not actual meta_block_start of block)
-              |
-              |
-          'payload' //('long aligned' blocks of size of user request)
-              |
-              | <- returned *ptr (actual start of free space)
-    prev avalible block (long)
-              |
-        block size (long) //(last bit of size would be the flag for malloced/ free, as per the ATAM lecture) (1 = allocated, 0 = free)
-      XX--other data--XX
+void* scalloc(size_t num, size_t size){
+    /*
+    ● Searches for a free block of at least ‘num’ elements, each ‘size’ bytes that are all set to 0
+    or allocates if none are found. In other words, find/allocate size * num bytes and set all
+    bytes to 0.
+    ● Return value:
+    i. Success - returns pointer to the first byte in the allocated block.
+    ii. Failure –
+    a. If size or num is 0 returns NULL.
+    b. If ‘size * num’ is more than 108, return NULL.
+    c. If sbrk fails in allocating the needed space, return NULL.
+    */
+}
 
-     */
+void sfree(void* p){
+    /*
+    ● Releases the usage of the block that starts with the pointer ‘p’.
+    ● If ‘p’ is NULL or already released, simply returns.
+    ● Presume that all pointers ‘p’ truly points to the beginning of an allocated block.
+    */
+}
 
-    if (size <= 0 || size > 100000000){
-        return nullptr;
-    }
+void* srealloc(void* oldp, size_t size){
+    /*
+    ● If ‘size’ is smaller than or equal to the current block’s size, reuses the same block.
+Otherwise, finds/allocates ‘size’ bytes for a new space, copies content of oldp into the
+new allocated space and frees the oldp.
+● Return value:
+i. Success –
+a. Returns pointer to the first byte in the (newly) allocated space.
+b. If ‘oldp’ is NULL, allocates space for ‘size’ bytes and returns a pointer to it.
+ii. Failure –
+a. If size is 0 returns NULL.
+b. If ‘size’ if more than 108, return NULL.
+c. If sbrk fails in allocating the needed space, return NULL.
+d. Do not free ‘oldp’ if srealloc() fails. 
+    */
+}
 
-    void* meta_block_start = sbrk(0);
+size_t _num_free_blocks(){
+    /*
+    ● Returns the number of allocated blocks in the heap that are currently free. 
+    */
+}
+size_t _num_free_bytes(){
+    /*
+    ● Returns the number of bytes in all allocated blocks in the heap that are currently free,
+excluding the bytes used by the meta-data structs. 
+    */
+}
 
-    if (SYSCALL_FAILED(meta_block_start)){
-        return nullptr;
-    }
+size_t _num_allocated_blocks(){
+    /*
+    ● Returns the overall (free and used) number of allocated blocks in the heap. 
+    */
+}
 
-    size_t alligned_size = ((size)+(size%ARCH_SIZE)); //allign size
+size_t _num_allocated_bytes(){
+    /*
+    ● Returns the overall number (free and used) of allocated bytes in the heap, excluding
+the bytes used by the meta-data structs. 
+    */
+}
 
-    size_t actual_size =  alligned_size + EXTRA_DATA_AMMOUNT*ARCH_SIZE;
+size_t _num_meta_data_bytes(){
+    /*
+    ● Returns the overall number of meta-data bytes currently in the heap. 
+    */
+}
 
-    void* one_after_meta_block_end = sbrk(actual_size);
-
-    if (SYSCALL_FAILED(one_after_meta_block_end)){
-        return nullptr;
-    }
-
-    void* meta_block_end = (one_after_meta_block_end - ARCH_SIZE);
-
-    //add actual size metadata to the edges of the blocks
-    *((size_t*)meta_block_start) = actual_size;
-    *((size_t*)meta_block_end) = actual_size;
-
-    size_t* pointer_to_prev_block = (((size_t*)meta_block_start) + 1*ARCH_SIZE);
-    size_t* pointer_to_next_block = (((size_t*)meta_block_end) - 1*ARCH_SIZE);
-
-    //nulify pointers to avoid garbage causing problems;
-    *pointer_to_prev_block = 0;
-    *pointer_to_next_block = 0;
-
-
-    void* payload_start = (meta_block_start + 2*ARCH_SIZE);
-
-    return payload_start;
-
+size_t _size_meta_data(){
+    /*
+    ● Returns the number of bytes of a single meta-data structure in your system.
+    */
 }
