7ccab0aff4c71011ce10674e48fa667e79918e87
guy friedman <guy.friedman2001@gmail.com>
2025-07-07 11:33:35 +0300

    Update malloc_2.cpp

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -1,8 +1,12 @@
 typedef unsigned long size_t; //FIXME: delete this line! its only for my mac
-void* smalloc(size_t size);
-void* scalloc(size_t num, size_t size);
-void sfree(void* p);
-void* srealloc(void* oldp, size_t size);
+
+typedef void* payload_start;
+typedef void* actual_block_start;
+
+payload_start smalloc(size_t size);
+payload_start scalloc(size_t num, size_t size);
+void sfree(payload_start p);
+payload_start srealloc(void* oldp, size_t size);
 size_t _num_free_blocks();
 size_t _num_free_bytes();
 size_t _num_allocated_blocks();
@@ -10,7 +14,12 @@ size_t _num_allocated_bytes();
 size_t _num_meta_data_bytes();
 size_t _size_meta_data();
 
-void* smalloc(size_t size){
+payload_start smalloc_helper_find_avalible(size_t size);
+actual_block_start actually_allocate(size_t size);
+inline void markFree(payload_start block);
+
+
+payload_start smalloc(size_t size){
     /*
     ● Searches for a free block with at least ‘size’ bytes or allocates (sbrk()) one if none are
 found.
@@ -22,7 +31,14 @@ a. If size is 0 returns NULL.
 b. If ‘size’ is more than 108, return NULL.
 c. If sbrk fails in allocating the needed space, return NULL. 
     */
+   payload_start return_pointer = smalloc_helper_find_avalible(size);
+
+   if (return_pointer != nullptr){
+    return return_pointer;
+   }
 }
+payload_start smalloc_helper_find_avalible(size_t size){}
+actual_block_start actually_allocate(size_t size){}
 
 void* scalloc(size_t num, size_t size){
     /*
@@ -98,4 +114,100 @@ size_t _size_meta_data(){
     /*
     ● Returns the number of bytes of a single meta-data structure in your system.
     */
+}
+
+
+enum MemoryStatus {
+    FREE = 0,
+    ALLOCATED = 1
+};
+
+typedef unsigned long size_t; //FIXME: delete this line! its only for my mac
+
+inline bool _isFlag(void* block, MemoryStatus flag){
+    if (!block){
+        //make it safe to call on nullptr, similar behaviour to dev/null
+        //as per the tutorials or lectures or whatever
+        return true;
+    }
+    //go 2 'objects' below the pointer
+    void* address_2_below = (block-2*ARCH_SIZE);
+
+    size_t value_at_addres_2_below = *((size_t*)address_2_below);
+    
+
+    //check if last bit is 1 or 0
+    return ((value_at_addres_2_below&flag)==flag);
+}
+
+inline bool isAllocated(void* block){
+    return _isFlag(block, ALLOCATED);
+}
+
+#if 0 // i dont think we need this function
+inline bool isFree(void* block){
+    return _isFlag(block, FREE);
+}
+#endif
+
+#if 0 //didnt find a good way to do it
+inline void _apply_to_flag(void* block, MemoryStatus flag){
+        if (!block){
+        //make it safe to call on nullptr, similar behaviour to dev/null
+        //as per the tutorials or lectures or whatever
+        return;
+    }
+
+    //go 2 'objects' below the pointer
+    size_t* address_2_below = (size_t*) (block-2*ARCH_SIZE);
+
+    ((*address_2_below)&=flag)|=flag;
+}
+#endif
+
+inline void markAllocated(void* block){
+    if (!block){
+        //make it safe to call on nullptr, similar behaviour to dev/null
+        //as per the tutorials or lectures or whatever
+        return;
+    }
+
+    //go 2 'objects' below the pointer
+    size_t* address_2_below = (size_t*) (block-2*ARCH_SIZE);
+
+    (*address_2_below)|=ALLOCATED;
+}
+
+inline void markFree(void* block){
+    if (!block){
+        //make it safe to call on nullptr, similar behaviour to dev/null
+        //as per the tutorials or lectures or whatever
+        return;
+    }
+
+    //go 2 'objects' below the pointer
+    size_t* address_2_below = (size_t*) (block-2*ARCH_SIZE);
+
+    //easiest way i found to make the first bit 0 without using a predefined register size,
+    //just make sure the first bit is 1 and then xor it with 1.
+    ((*address_2_below)|=ALLOCATED)^=ALLOCATED;
+}
+
+inline size_t getBlockSize(void* block){
+    if (!block){
+        //make it safe to call on nullptr, similar behaviour to dev/null
+        //as per the tutorials or lectures or whatever
+        return 0;
+    }
+
+    //go 2 'objects' below the pointer
+    size_t* address_2_below = (size_t*) (block-2*ARCH_SIZE);
+
+    size_t size = *address_2_below;
+
+    //easiest way i found to make the first bit 0 without using a predefined register size,
+    //just make sure the first bit is 1 and then xor it with 1.
+    ((size)|=ALLOCATED)^=ALLOCATED;
+
+    return size;
 }
